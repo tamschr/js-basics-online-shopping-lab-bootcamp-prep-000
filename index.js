1457e46756091e3dfaabb1b72836f8641cad8194
c.tams@outlook.com <c.tams@outlook.com>
2018-06-02 18:47:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ function viewCart() {
     }
         if (cart.length === 2){
           
-      return ("In your cart, you have " + cart[0].itemName + " at " + "$" + cart[0].itemPrice + " and " + cart[1].itemName + " at " + cart[1].itemPrice + ".");
+      return ("In your cart, you have " + cart[0].itemName + " at " + "$" + cart[0].itemPrice + ", and " + cart[1].itemName + " at " + cart[1].itemPrice + ".");
     }
     else{
     return ("In your cart, you have " + cart1 + ".");
